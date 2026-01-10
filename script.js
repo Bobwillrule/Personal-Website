75137e3199383847dcbacbe7d50bbb1986e13262
bobwillrule <hugohqchen@gmail.com>
2026-01-09 18:47:11 -0800

    white top fix

--- script.js
+++ script.js
@@ -4,8 +4,11 @@ const globalMsg = document.querySelector("#global");
 window.addEventListener("scroll", () => {
     if (window.scrollY > 50) {
         nav.classList.add("scrolled");
-        globalMsg.remove()
     } else {
         nav.classList.remove("scrolled");
+    }
+
+    if (window.scrollY > 100){
+        globalMsg.remove()
     }
 })
