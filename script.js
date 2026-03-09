e75ab125dcb5d9c8b334f8a7a777bd12104cf3ff
bobwillrule <hugohqchen@gmail.com>
2026-03-09 14:12:52 -0700

    refactoring

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
+// Select the navigation bar element from the DOM
 const nav = document.querySelector("nav");
+
+// Select the globar message bar
 const globalMsg = document.querySelector("#global");
 
+
 window.addEventListener("scroll", () => {
+
+    // if scroll more than 50 px down the page
     if (window.scrollY > 50) {
-        nav.classList.add("scrolled");
+        nav.classList.add("scrolled"); // unstick nav
     } else {
-        nav.classList.remove("scrolled");
+        nav.classList.remove("scrolled"); // stick back nav
     }
 
+    // if user scroll more than 100 px or more
     if (window.scrollY > 100){
-        globalMsg.remove()
+        globalMsg.remove() // remove the global banner
     }
 })
